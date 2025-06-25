6a23d0dc92187c6531cae2e6a760fc36d3a9d19a
Muhammad Farhan <=>
2025-06-25 12:25:08 +0700

    modifikasi firebase.js dan index.js

--- farhan-backend/src/index.js
+++ farhan-backend/src/index.js
@@ -1,24 +1,18 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import contactRoutes from './routes/contact.js';
-
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
 const contactRoutes = require('./routes/contact.js')
 
-dotenv.config();
+dotenv.config(); // Panggil di awal
+console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID); // Debug
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use('/api/contact', contactRoutes);
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
