05cf7ed7ce7c67fd181a878286d48713089813ed
Muhammad Farhan <=>
2025-08-18 12:30:43 +0700

    chore: stop tracking .next build output

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/media/stooner/New Volume/Belajar Web/Javascript/NextJs/projects/farhan/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
